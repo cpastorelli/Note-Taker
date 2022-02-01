28fb346cf660bd4aad6b4b7358c16453d3e003f5
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-01-31 18:23:49 -0800

    created gitignore file, began apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,8 @@
+// brings in the module from NPM 
+// and runs router function that returns a router object
+const router = require('express').Router();
+
+
+
+// export to use in server file
+module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 // set up Express Server
 const express = require('express');
 
+// brought in routes from routes folders
+const apiRoutes = require('./routes/apiRoutes');
+
+
 // initializing the applicaion & setting the port
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-// for POST and PUT requests: 
-// asking server toaccept or sstore the data enclosed in the body (req.body) of that request
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+
+// Serve static files from public folder (HTML, CSS, IMG, ECT)
 app.use(express.static('public'));
 
+// middleware to access base routes with /api to be routed to apiRoutes
+app.use('/api', apiRoutes);
 
 // begins the server on the port number. Shows the port number in the console.log
 app.listen(PORT, () => console.log(`View me on port: ${PORT}!`));
