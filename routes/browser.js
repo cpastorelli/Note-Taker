40910d45cc9e9c4f8bbd1c930afdf548588f43c4
Caroline Pastorelli <cpastorelli@arcanabrewing.com>
2022-03-13 18:52:17 -0700

    changed variable names in browser.js

--- routes/browser.js
+++ routes/browser.js
@@ -1,16 +1,16 @@
 // brings in the module from NPM 
 // and runs router function that returns a router object
-const browser = require('express').Router();
+const router = require('express').Router();
 const path = require('path');
 
 
 // when going to /notes in browser, notes.html will be loaded
-browser.get('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 });
 
 // testing wildcard setting for any alternative paths
-browser.get('*', (req,res) => {
+router.get('*', (req,res) => {
     console.log(`unidentified path accessed, redirected to index.html`);
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
